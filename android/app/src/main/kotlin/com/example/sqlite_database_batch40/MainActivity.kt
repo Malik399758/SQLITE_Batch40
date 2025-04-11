f3259a264ae90ffb84eba49caf2dc1952fd7d1f9
Abidcomputers <ym399758@gmail.com>
2025-04-10 21:31:44 -0700

    Model completed.

---
+++ android/app/src/main/kotlin/com/example/sqlite_database_batch40/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sqlite_database_batch40
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/user_model/user-model.dart
@@ -0,0 +1,50 @@
+
+// Create a model
+
+class UserModel{
+  // declare variables
+
+  final String title;
+  final String description;
+
+  // constructor
+
+ UserModel({required this.title,required this.description});
+
+ // convert into map
+
+ Map<String,dynamic> toMap(){
+   return {
+     'title' : title,
+     'description' : description,
+   };
+
+ }
+
+ // convert from map
+
+ factory UserModel.fromMap(Map<String,dynamic> map){
+   return UserModel(
+       title: map['title'],
+       description: map['description']);
+ }
+
+
+} // end of class
+
+// main function
+void main(){
+  var userModel = UserModel(title: 'Yaseen Malik', description: 'This is me Yaseen Malik');
+
+  final result = userModel.toMap();
+
+  print(result);
+
+}
+
+
+
+
+
+
+
